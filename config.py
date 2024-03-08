9541ab09aae3cbe8cfec456d1be6b480fcacf4c0
Gagan <business@devgagan.in>
2024-03-08 18:56:17 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = "26482070"
+API_HASH = "28f1d67e2693e057ae4611521c555136"
+
+BOT_TOKEN = "6557432874:AAGywcrSYYwKE9o_r3foRzfVM6khPLgPMxk"
+OWNER_ID = "6441456023"
+
+MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
+MUST_JOIN = "dev_gagan"
